3ae1c95647235612e6928fe437fb8f6dc6bf0838
vidushalakshan <vidushalakshan7890@gmail.com>
2025-03-26 15:29:21 +0530

    updated nav content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {appleImg} from '../utils/index'
+import {appleImg, bagImg, searchImg} from '../utils/index'
 
 const Navbar = () => {
   return (
@@ -6,6 +6,19 @@ const Navbar = () => {
         <nav>
             <img src={appleImg} alt="appleLogo" width={14} height={18} />
         </nav>
+
+        <div>
+            {['Phones', 'Macbook', 'Talets'].map((nav) => (
+                <div key={nav}>
+                    {nav}
+                </div>
+            ))}
+        </div>
+
+        <div>
+            <img src={searchImg} alt="Searchicon"  height={18} width={18}/>
+            <img src={bagImg} alt="bagicon"  height={18} width={18}/>
+        </div>
     </header>
   )
 }
