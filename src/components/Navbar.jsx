236962443674d296acbfd24981eea9c7af4f76c4
vidushalakshan <vidushalakshan7890@gmail.com>
2025-03-26 14:40:25 +0530

    aded apple logo in nav bar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import {appleImg} from '../utils/index'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <img src={appleImg} alt="appleLogo" width={14} height={18} />
+        </nav>
+    </header>
   )
 }
 
