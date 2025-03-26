6460ef3a0569c5b869f9b263d92e945b1122cda8
vidushalakshan <vidushalakshan7890@gmail.com>
2025-03-26 14:21:27 +0530

    start apple 3d website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='bg-black'>
+      Navbar 
+      Hero 
+      Highlights 
+      
+    </main>
+  )
+}
+
+export default App
+  
