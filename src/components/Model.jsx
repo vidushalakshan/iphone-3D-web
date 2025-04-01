bf0323f8e125cf7eb4fb5ee13861e8795c905270
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-01 17:29:02 +0530

    updated closer name content

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
 
 const Model = () => {
+
+    useGSAP(() => {
+        gsap.to('#heading', { y:0, opacity: 1})
+    }, []);
+
   return (
-    <section>
-        <div>
-            <h1>Take a closer look.</h1>
+    <section className='common-padding'>
+        <div className='screen-max-width'>
+            <h1 id='heading' className='section-heading'>Take a closer look.</h1>
+            
         </div>
     </section>
   )
