3977875836e1828e730e5d98e2e1a94a7cb702b5
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-01 17:23:39 +0530

    start Model Section part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
       <Hero /> 
       <Highlights />
-
+      <Model />
     </main>
   )
 }
---
+++ src/components/Model.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Model = () => {
+  return (
+    <section>
+        <div>
+            <h1>Take a closer look.</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Model
